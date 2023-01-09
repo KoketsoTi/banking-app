3090f45835a32382c1933b64686362bc85c9b8ba
Excellent <excellent.mashengete@dadevs.co.za>
2023-01-09 16:14:17 +0200

    updates

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -11,7 +11,7 @@ function App() {
 <input type="checkbox" id="my-modal" className="modal-toggle" />
 <div className="modal">
   <div className="modal-box">
-    <h3 className="font-bold text-lg">Congratulations random Internet user!</h3>
+    <h3 className="font-bold text-lg">Congratulations</h3>
     <p className="py-4">You've been selected for a chance to get one year of subscription to use Wikipedia for free!</p>
     <div className="modal-action">
       <label htmlFor="my-modal" className="btn">Yay!</label>
