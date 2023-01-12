acfa3c7d70b5e5eed7df726abb30728d9987e651
KoketsoTi <bettytinyane@gmail.com>
2023-01-12 15:50:27 +0200

    forgot password

--- backend/config/plugins.js
+++ backend/config/plugins.js
@@ -44,24 +44,4 @@ module.exports = ({ env }) => ({
 
   });
 
-  /*  
   
-   email: {
-      config: {
-        provider: 'nodemailer',
-        providerOptions: {
-          host: env('SMTP_HOST', 'smtp.gmail.com'),
-          port: env('SMTP_PORT', 587),
-          auth: {
-            user: "koki.tinyane@gmail.com",
-            pass: "klxpscraffdnjoze"
-          },
-          // ... any custom nodemailer options
-        },
-        settings: {
-          defaultFrom: "koki.tinyane@gmail.com",
-          defaultReplyTo: "koki.tinyane@gmail.com",
-        },
-      },
-    },
-  */
--- frontend/src/components/admin/Login/Login.jsx
+++ frontend/src/components/admin/Login/Login.jsx
@@ -1,50 +1,72 @@
 import './Login.css';
-import React, { useState } from 'react';
+import React, { useRef } from 'react';
 import Container from 'react-bootstrap/Container';
 import { ToastContainer } from 'react-toastify';
 import { Success, Warning, Error } from '../../../helpers/toasters';
-import { FaSignInAlt} from 'react-icons/fa';
+import { FaSignInAlt } from 'react-icons/fa';
 import ForgotPassword from '../../../Models/forgotPasswordModel';
 import axios from 'axios';
 import { API } from '../../../environment/constant';
 import { setToken } from "../../../helpers/helpers";
+import { useFormInputValidation } from "react-form-input-validation";
+
+
+
 
 function Login() {
-    const [identifier, setIdentifier] = useState('');
-    const [password, setPassword] = useState('');   
-    
-    let login = async(e)=>{
-        e.preventDefault();
-        try {
-            await axios.post(`${ API}auth/local`, {
-                identifier: identifier,
-                password: password
-            })
-            .then(response => { 
-                // console.log('User profile', response.data.user);
-                // console.log('User token', response.data.jwt);
-                // set the token
-                setToken(response.data.jwt);
-        
-                // set the user
-                // setUser(response.data.user);
+    const forms = useRef();
+    const [fields, errors, form] = useFormInputValidation({
+        identifier: "",
+        password: "",
+    }, {
+        identifier: "required|email",
+        password: "required|min:8"
+    });
 
-                Success('Successfuly logged in')
+
+
+    // const [identifier, setIdentifier] = useState('');
+    // const [password, setPassword] = useState('');
+
+   async function login (e)  {
+
+        e.preventDefault();
+
+        console.log(fields)
+ 
+        const isValid = await form.validate(e)
+
+    console.log(isValid)
+
+        if (isValid) {
+         
+            await axios.post(`${API}auth/local`, {
+                identifier: fields.identifier,
+                password: fields.password
             })
-            .catch(error => {  
-                console.log('An error occurred:', error.response);
-                Warning('Incorrect email or password entered')
-            });
-        } catch (error) {
-            console.error(error);
-            Error('Something went wrong!')
-        } 
+                .then(response => {
+                    // console.log('User profile', response.data.user);
+                    console.log('User token', response.data.jwt);
+                    // set the token
+                    setToken(response.data.jwt);
+
+                    // set the user
+                    // setUser(response.data.user);
+
+                    Success('Successfuly logged in')
+                })
+                .catch(error => {
+                    console.log('An error occurred:', error.response);
+                    Warning('Incorrect email or password entered')
+                });
+
+        }
     }
 
     return (
         <Container className='login' >
             <ToastContainer />
-            
+
             <div className="md:container md:mx-auto">
                 <div className="flex justify-center">
                     <div className="card lg:xl:1/2 w-96 rounded-none shadow-xl ">
@@ -54,21 +76,48 @@ function Login() {
                             </div>
 
                             <div className="hozitontal-line -mb-4">
-                                <div className="divider"></div> 
+                                <div className="divider"></div>
                             </div>
-                 
-                            <form onSubmit={login}>
+
+                            <form noValidate  ref={forms} >
                                 <div className="form-group col mb-4">
                                     <label className="label"><span className="label-text">USERNAME OR EMAIL</span></label>
-                                    <input type="email" name="identifier"  placeholder="Email" value= {identifier} onChange={(e)=>{ setIdentifier(e.target.value) }}
-                                        className="input input-bordered w-full max-w-s email "/>
+                                    <input
+                                     type="email"
+                                     name="identifier"
+                                      placeholder="Email"
+                                     onBlur={form.handleBlurEvent}
+                                     required
+                                        onChange={form.handleChangeEvent}
+                                        value={fields.identifier}
+                                        className="input input-bordered w-full max-w-s email "
+                                        />
+
+                                    <label className="error">
+                                        {errors.identifier
+                                            ? errors.identifier
+                                            : ""}
+                                    </label>
+                                    
                                 </div>
 
                                 <div className="form-group col mb-4">
                                     <label className="label"><span className="label-text">PASSWORD</span></label>
-                                    <input type="password" name="password" placeholder="Password" value= {password} onChange={(e)=>{ setPassword(e.target.value)}}
-                                        className="input input-bordered w-full max-w-s email "/>
+                                    <input type="password" name="password" placeholder="Password" onBlur={form.handleBlurEvent}
+                                        onChange={form.handleChangeEvent}
+                                        value={fields.password}
+                                        minLength={8}
+                                        maxLength={16}
+                                    
+                                        className="input input-bordered w-full max-w-s email "
+                                        required />
+                                    <label className="error">
+                                        {errors.password
+                                            ? errors.password
+                                            : ""}
+                                    </label>
                                 </div>
+
 
                                 <div className="form-group col mb-4">
                                     <label htmlFor="my-modal-4" className="forgot" >Forgot Password</label>
@@ -76,7 +125,7 @@ function Login() {
                                 </div>
 
                                 <div className="form-group col mb-8">
-                                    <button type="submit" className="rounded-none relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"><FaSignInAlt style={{marginTop: "3px", marginRight:"5px"}}/>Login </button>
+                                    <button onClick={login} className="rounded-none relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"><FaSignInAlt style={{ marginTop: "3px", marginRight: "5px" }} />Login </button>
                                 </div>
                             </form>
                         </div>
