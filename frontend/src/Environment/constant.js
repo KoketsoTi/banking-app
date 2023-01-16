69cbc36a7c0caa5650cfec56e24090d921c01f44
Excellent <excellent.mashengete@dadevs.co.za>
2023-01-16 10:12:50 +0200

    top navnbar

--- frontend/src/Environment/constant.js
+++ frontend/src/Environment/constant.js
@@ -1,3 +1,7 @@
 export const API = "http://localhost:1337/api/";
 export const AUTH_TOKEN = "authToken";
+export const EMAIL = "email";
+export const FIRSTNAME = "firstname";
+export const LASTNAME = "lastname";
+export const PHONENUMBER = "phone";
 export const BEARER = "Bearer";
