11ef2470128810756db0db8dcd3f36fbfdb2ecdd
KoketsoTi <bettytinyane@gmail.com>
2023-01-17 11:33:50 -0800

    pulled develop

--- frontend/src/Components/Admin/Loans/Long-term.jsx
+++ frontend/src/Components/Admin/Loans/Long-term.jsx
@@ -5,6 +5,7 @@ import { dataLoans } from '../../../Data/mockedData';
 import { AiOutlineEye, AiOutlineCloseCircle } from 'react-icons/ai';
 import { useNavigate } from 'react-router-dom';
 
+
 function LongTerm(){
     const navigate = useNavigate();
 
