7fc07052d4b812bd8248e6f3b8d13ba3eb1ddd07
Excellent <excellent.mashengete@dadevs.co.za>
2023-01-25 08:56:59 +0200

    added fridah and fixed logged in user

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,34 +1,11 @@
 import './App.css';
-import { useState, useEffect,  } from 'react';
-import { UserContext } from './Authorization/userContext';
-import { getToken  } from "./Helpers/helpers";
 import AdminRoutes from './Routes';
-import AuthorService from "./Service/auth.service";
 
 function App() {
-  const [user, setUser] = useState(null);
-  const authToken = getToken();
-
-  useEffect(() => {
-    if(authToken){
-      AuthorService.loggedInUser(authToken).then((response) => { 
-        //set userData in a useContext 
-        setUser(response.data.user);
-      })
-      .catch((error) => {  
-        console.log('An error occurred:', error.response);
-      });
-    }
-  }, []);
-
-
   return (
-    <UserContext.Provider value={{user, setUser}} >
-      <div className='App'>
-        <AdminRoutes />
-      </div>
-    </UserContext.Provider>
-
+    <div className='App'>
+      <AdminRoutes />
+    </div>
   );
 }
 export default App;
